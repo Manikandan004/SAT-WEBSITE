bc44d243c1d7e5792592b5c766a1384ab117cdec
Your Name <you@example.com>
2023-08-27 23:34:27 +0530

    commit header

--- sat/satkarur/src/app/app-routing.module.ts
+++ sat/satkarur/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:HeaderComponent,
+    path:"header"
+  },
+  {
+    component:HomepageComponent,
+    path:"home"
+  },
+  {
+    component:AboutusComponent,
+    path:"about"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sat/satkarur/src/app/app.module.ts
+++ sat/satkarur/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CategoryComponent } from './category/category.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    AboutusComponent,
+    GalleryComponent,
+    CategoryComponent,
+    ContactusComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
